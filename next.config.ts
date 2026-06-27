8b11ce6b33def47229b6278720d2a0c745f39338
faqih <faqih.rachman@inteleq.id>
2026-06-27 11:35:41 +0700

    Configure Next.js for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  // Hapus komentar di bawah jika Anda menggunakan URL github.io/portofilio-mine (tanpa custom domain)
+  // basePath: "/portofilio-mine",
 };
 
 export default nextConfig;
